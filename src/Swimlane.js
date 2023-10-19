f58efffa8721b02c5c34d9d06433a1a15219b8da
Zaibonisha <zsmohamed94@gmail.com>
2023-10-19 14:17:52 +0200

    Changes

--- src/Swimlane.js
+++ src/Swimlane.js
@@ -1,27 +1,53 @@
-import React from 'react';
-import Card from './Card';
-import './Swimlane.css';
+import React, { Component } from 'react';
+import Swimlane from './Swimlane';
 
-export default class Swimlane extends React.Component {
-  render() {
-    const cards = this.props.clients.map(client => {
-      return (
-        <Card
-          key={client.id}
-          id={client.id}
-          name={client.name}
-          description={client.description}
-          status={client.status}
-        />
-      );
-    })
-    return (
-      <div className="Swimlane-column">
-        <div className="Swimlane-title">{this.props.name}</div>
-        <div className="Swimlane-dragColumn" ref={this.props.dragulaRef}>
-          {cards}
-        </div>
-      </div>);
+export default class ParentComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    // Initialize state with cards and their swimlanes
+    this.state = {
+      swimlanes: [
+        {
+          name: 'Backlog',
+          cards: [
+            { id: 1, name: 'Task 1', description: 'Description 1', status: 'backlog' },
+            // Add more cards
+          ],
+        },
+        {
+          name: 'In Progress',
+          cards: [
+            { id: 2, name: 'Task 2', description: 'Description 2', status: 'in-progress' },
+            // Add more cards
+          ],
+        },
+        {
+          name: 'Complete',
+          cards: [
+            { id: 3, name: 'Task 3', description: 'Description 3', status: 'complete' },
+            // Add more cards
+          ],
+        },
+      ],
+    };
   }
 
+  // Implement functions to update swimlanes and card statuses here
+
+  render() {
+    return (
+      <div>
+        {this.state.swimlanes.map(swimlane => (
+          <Swimlane
+            key={swimlane.name}
+            name={swimlane.name}
+            cards={swimlane.cards}
+            // Pass a function to update swimlanes and card statuses
+            updateSwimlanesAndStatus={this.updateSwimlanesAndStatus}
+          />
+        ))}
+      </div>
+    );
+  }
 }
